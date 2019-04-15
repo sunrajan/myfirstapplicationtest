a1068ebf2bc4d8027574a7b79a7886c676827587
sunrajan <49509950+sunrajan@users.noreply.github.com>
2019-04-15 15:59:50 +0530

    update

--- src/main/java/springboot/myfirstapplicationtest/controller/testcontroller.java
+++ src/main/java/springboot/myfirstapplicationtest/controller/testcontroller.java
@@ -2,9 +2,7 @@ package springboot.myfirstapplicationtest.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 
 @RestController
 public class testcontroller {
