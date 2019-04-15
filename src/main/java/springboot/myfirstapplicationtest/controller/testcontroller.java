429b387494cda5b93f8d3e054640afd6cb5b5bbf
sunrajan <49509950+sunrajan@users.noreply.github.com>
2019-04-15 17:34:30 +0530

    Update testcontroller.java

--- src/main/java/springboot/myfirstapplicationtest/controller/testcontroller.java
+++ src/main/java/springboot/myfirstapplicationtest/controller/testcontroller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testcontroller {
 
-	@RequestMapping("/")
+	@RequestMapping("")
 
 	public String home() {
 
