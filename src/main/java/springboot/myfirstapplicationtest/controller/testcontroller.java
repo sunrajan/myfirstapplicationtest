5e529d18f69d3bdc17f0fb26c50f56ad9ab281a6
sunrajan <49509950+sunrajan@users.noreply.github.com>
2019-04-16 17:03:59 +0530

    updates with couchbase jars

--- src/main/java/springboot/myfirstapplicationtest/controller/testcontroller.java
+++ src/main/java/springboot/myfirstapplicationtest/controller/testcontroller.java
@@ -1,19 +1,52 @@
 package springboot.myfirstapplicationtest.controller;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import com.ual.des.cbutils.Couchbaseutil;
 
+import com.couchbase.client.core.config.ConfigurationException;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.RawJsonDocument;
+import org.apache.log4j.Logger; 
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
 
 @RestController
 public class testcontroller {
+	
+	public static final String CB_URL = "http://localhost";
+    public static final String UCD_BUCKET = "UFD";
+    public static final String UCD_BUCKET_PASS = "ufd123";
+    private static Logger logger = Logger.getLogger(Couchbaseutil.class);
 
 	@RequestMapping("")
 
 	public String home() {
+		
+		 Couchbaseutil.init(CB_URL);
+	        Couchbaseutil.initBucket(UCD_BUCKET,UCD_BUCKET_PASS);
+
+	        String jsonStr = "{\"testsprintFlt\": \"test springbootvalue\"}";
+	        String key = "testspringboot";
+	        int expirationInSecs = 100;
+	     //   Bucket bucket = Couchbaseutil.getBucket(UCD_BUCKET);
+
+	       // Couchbaseutil.deleteDoc(bucket, key);
+
+	       // RawJsonDocument doc = Couchbaseutil.getDoc(bucket, key, true);
+	      //  assertThat(doc, is(equalTo(null)));
+	        
 
 		System.out.println(" Log section with out context or /");
+		
+		
 		
 	return "Spring boot is working! great !!";
 
